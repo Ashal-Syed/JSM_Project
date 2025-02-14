314aa05a2b2e68220679c8e25026866f00a5ca4c
Ashal-Syed <ashal.syed@gmail.com>
2025-02-15 03:36:10 +1100

    Finished Startup Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home({ searchParams }: {
     {
       _createdAt: new Date(),
       Views: 55,
-      author: { _id: 1 },
+      author: { _id: 1, name: 'Ashal' },
       _id : 1,
       description: "This is a description",
       image: "https://images.unsplash.com/photo-1581090464777-f3220bbe1b8b?q=80&w=2432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
