b1074525a2363dd2b9464ab2050de1244a79d7f6
Ashal-Syed <ashal.syed@gmail.com>
2025-02-14 22:26:15 +1100

    started StartupCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({ searchParams }: {
     searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: "Yesterday",
+      Views: 55,
+      author: { _id: 1 },
+      _id : 1,
+      description: "This is a description",
+      image: "https://images.unsplash.com/photo-1581090464777-f3220bbe1b8b?q=80&w=2432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots"
+    }];
+
+
   return (
      <>
       <section className="pink_container">
@@ -16,6 +31,28 @@ export default async function Home({ searchParams }: {
         <SearchForm query={query}/>
 
       </section>
+
+      <section className="section_container"> 
+        <p className="text-30-semibold">
+          {query? `Search Results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post}  />
+          ))
+          ) : (
+           <p className="no-results">No Startups Found</p> 
+          )}
+
+        </ul>
+      </section>
+
+      <div className="bg-blue-500 text-white p-5">
+        If you see this, Tailwind is working!
+      </div>
+
         
      </>
   );
---
+++ components/StartupCard.tsx
@@ -0,0 +1,10 @@
+
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  return (
+    <li className="startup-card group">
+        Startup List
+    </li>
+  )
+}
+
+export default StartupCard
